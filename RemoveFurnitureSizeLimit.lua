94cbaecb223dcd70293cc1222568e6fa239aaaef
l3a <l3abeans@protonmail.com>
2024-05-31 21:19:52 -0400

    Removed furniture size limit

---
+++ RemoveFurnitureSizeLimit.lua
@@ -0,0 +1,18 @@
+-- ======================================================================
+-- Removes 100 block furniture editor limit
+-- ======================================================================
+writeBytes("MySims.exe+207BC4", 235)
+    -- jl MySims.exe+207C01 
+    -- v
+    -- jmp MySims.exe+207C01 
+    -- (skips too many blocks error that puts away block selector)
+writeBytes("MySims.exe+206B8C", 144)
+    -- jnl Address
+    -- v
+    -- nop nop
+    -- (skips too many blocks error that puts arrows away)
+writeBytes("MySims.exe+206B8C", 144)
+    -- jnl Address
+    -- v
+    -- nop nop
+    -- (skips too many block error that makes going to eccense mode break)
